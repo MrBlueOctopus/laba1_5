5f9e720e4a8d6b89564d0160704465c6645edd8b
MrBlueOctopus <70705348+MrBlueOctopus@users.noreply.github.com>
2020-09-03 19:30:17 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Факторил какого числа нужно вычеслить?");
+	Scanner in = new Scanner(System.in);
+	int a=in.nextInt(), fac=1;
+	for (int i=1; i<=a; ++i){
+	    fac*=i;
+    }
+	System.out.println(fac);
+    }
+}
